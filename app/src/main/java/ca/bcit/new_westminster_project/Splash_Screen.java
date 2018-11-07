75bd363d79c222df2f2e4698a4588b86a5d618a0
Erwin Berceles <eberceles1@my.bcit.ca>
2018-11-07 13:45:36 -0800

    Started Splash Screen

---
+++ app/src/main/java/ca/bcit/new_westminster_project/Splash_Screen.java
@@ -0,0 +1,13 @@
+package ca.bcit.new_westminster_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+    }
+}
